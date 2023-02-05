30047793cc9fa6c3c170b023a8d330ab824e2929
Gabriel Gaede <gabriel.gaede@troika.ind.br>
2023-02-05 18:39:20 -0300

    Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,16 @@
 import { Header } from "@/components/Headers";
+import { Sidebar } from "@/components/Sidebar";
+import { Flex } from "@chakra-ui/react";
 
 export default function Dashboard() {
     return (
-        <Header/>
+        <Flex direction="column" height="100vh">
+            <Header/>
+
+            <Flex width="100%" marginY="6" maxWidth={1480} marginX="auto" paddingX="6">
+                <Sidebar></Sidebar>
+            </Flex>
+        </Flex>
+
     );
 }
